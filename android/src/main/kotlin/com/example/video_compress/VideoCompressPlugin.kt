537f9f9011678c5e5479092523e286477e81accb
Miloš Karakaš <34490121+MilosKarakas@users.noreply.github.com>
2021-11-09 15:39:40 +0100

    Update VideoCompressPlugin.kt
    Fix

--- android/src/main/kotlin/com/example/video_compress/VideoCompressPlugin.kt
+++ android/src/main/kotlin/com/example/video_compress/VideoCompressPlugin.kt
@@ -11,10 +11,10 @@ import com.otaliastudios.transcoder.strategy.DefaultAudioStrategy
 import com.otaliastudios.transcoder.strategy.DefaultVideoStrategy
 import com.otaliastudios.transcoder.strategy.RemoveTrackStrategy
 import com.otaliastudios.transcoder.strategy.TrackStrategy
-import com.otaliastudios.transcoder.strategy.size.*
+import com.otaliastudios.transcoder.common.*
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
-import com.otaliastudios.transcoder.internal.Logger
+import com.otaliastudios.transcoder.internal.utils.Logger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
